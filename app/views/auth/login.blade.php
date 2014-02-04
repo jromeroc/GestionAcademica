07202b1b56dd88d90bbcd23d4518801d1d7e398e
jcromeroc <jcamiloromeroc@gmail.com>
2014-02-03 22:08:01 -0500

    Ajustes formulario login

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -15,27 +15,27 @@
 {{ Form::open(array('url' => 'login', 'class' => 'form-horizontal')) }}
 
     <!-- Name -->
-    <div class="control-group {{{ $errors->has('username') ? 'error' : '' }}}">
-        {{ Form::label('username', 'Usuario', array('class' => 'control-label')) }}
-        <div class="controls">
-            {{ Form::input('text', 'username', '', array('placeholder'=>'Usuario'))}}
+    <div class="form-group {{{ $errors->has('username') ? 'error' : '' }}}">
+        {{ Form::label('username', 'Usuario', array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-2">
+            {{ Form::input('text', 'username', '', array('class'=>'form-control','placeholder'=>'Usuario'))}}
             {{ $errors->first('username') }}
         </div>
     </div>
 
      <!-- Password -->
-    <div class="control-group {{{ $errors->has('password') ? 'error' : '' }}}">
-        {{ Form::label('password', 'Contraseña', array('class' => 'control-label')) }}
-        <div class="controls">
-            {{ Form::input('password', 'password', '', array('placeholder'=>'Contraseña'))}}
+    <div class="form-group {{{ $errors->has('password') ? 'error' : '' }}}">
+        {{ Form::label('password', 'Contraseña', array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-2">
+            {{ Form::input('password', 'password', '', array('class'=>'form-control','placeholder'=>'Contraseña'))}}
             {{ $errors->first('password') }}
         </div>
     </div>
 
     <!-- Login button -->
-    <div class="control-group">
-        <div class="controls">
-            {{ Form::submit('Iniciar sesión', array('class' => 'btn')) }}
+    <div class="form-group">
+         <div class="col-sm-offset-2 col-sm-3">
+            {{ Form::submit('Iniciar sesión', array('class' => 'btn btn-success')) }}
         </div>
     </div>
 
