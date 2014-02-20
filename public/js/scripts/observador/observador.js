b8b368728ff0208f4531f63b6c7a0c0971734a70
JJDJ <librajohan2000@gmail.com>
2014-02-19 20:28:54 -0500

    Avances Observador funcion nuevo con alumnos

--- public/js/scripts/observador/observador.js
+++ public/js/scripts/observador/observador.js
@@ -1,4 +1,7 @@
+var dataAlums;
+
 $(document).on('ready',function(){
+	
 	$('#slt_list_grupo').on('click',function(){
 		$.ajax({
 			url:root+'observador/grupo/'+$('#grupo').val(),
@@ -7,8 +10,47 @@ $(document).on('ready',function(){
 			{
 				$('#list-body').html(data);
 				$('#modal-list').modal('show');
+				selectAll();
+				$('#ProcessD').on('click',function(){
+					//$('input[name="select_all[]"]').serialize();
+					dataAlums = $('input[name="alums[]"]').serializeArray();
+					//alert(dataAlums.length);
+					console.log(dataAlums);
+					$('#modal-list').modal('hide');
+					
+					$('#msnalum').html(dataAlums.length+' Alumnos seleccionados');
+					/*
+					for(var i in dataAlums)
+					{
+						for (var j in i)
+						{
+  							alert('El valor de arr['+i+'] = ' + i[j]);
+							
+						}
+  					}
+  					*/
+				});
+				
 			}
 
 		},'html');
+
 	});
+	
 });
+
+
+function selectAll()
+	{
+		$('#select_all').on('change',function(){
+			if($(this).is(":checked")){
+				
+		 		$(".ck").prop("checked", "checked");
+		 		
+		 	}
+		 	else{
+				$(".ck").removeAttr("checked");
+				
+		 	}
+		});
+	}
